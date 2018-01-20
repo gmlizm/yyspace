49876ba2abb95597a63c9b60b3732f5a08b5acb9
gmlizm <gmlizm@gmail.com>
2018-01-20 11:55:11 -0500

    modify copy shell

--- yypull.sh
+++ yypull.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
-REMOTE_HOST=10.1.1.186
+REMOTE_HOST=10.1.1.134
+REMOTE_USER=root
 REMOTE_BASEDIR=/mnt/yyspace
-LOCAL_BASEDIR=/data/yyspace-
+LOCAL_BASEDIR=/data/yyspace
 
 
 echo "### START!"
 
 echo "====copy knowledge*.tar.gz"
-scp yyuser1@${REMOTE_HOST}:$REMOTE_BASEDIR/knowledge/ext/knowledge*.tar.gz $LOCAL_BASEDIR/knowledge/ext
+scp $REMOTE_USER@${REMOTE_HOST}:$REMOTE_BASEDIR/knowledge/ext/knowledge*.tar.gz $LOCAL_BASEDIR/knowledge/ext
 
 echo "====copy systemcenter*.tar.gz"
-scp yyuser1@${REMOTE_HOST}:$REMOTE_BASEDIR/sys/ext/systemcenter*.tar.gz $LOCAL_BASEDIR/apps/sys/ext
+scp $REMOTE_USER@${REMOTE_HOST}:$REMOTE_BASEDIR/sys/ext/systemcenter*.tar.gz $LOCAL_BASEDIR/apps/sys/ext
 
 echo "====copy engine*.tar.gz"
-scp yyuser1@${REMOTE_HOST}:$REMOTE_BASEDIR/engine/ext/engine*.tar.gz $LOCAL_BASEDIR/apps/engine/ext
+scp $REMOTE_USER@${REMOTE_HOST}:$REMOTE_BASEDIR/engine/ext/engine*.tar.gz $LOCAL_BASEDIR/apps/engine/ext
 
 echo "====copy med_all*.tar.gz"
-scp yyuser1@${REMOTE_HOST}:$REMOTE_BASEDIR/med/ext/med_all*.tar.gz $LOCAL_BASEDIR/apps/med/ext
+scp $REMOTE_USER@${REMOTE_HOST}:$REMOTE_BASEDIR/med/ext/med_all*.tar.gz $LOCAL_BASEDIR/apps/med/ext
 
-echo "====copy med.war"
-scp yyuser1@${REMOTE_HOST}:$REMOTE_BASEDIR/soft/tomcat-med/webapps/med.war $LOCAL_BASEDIR/apps/med
-
-echo "====copy knowledge ROOT.war"
-scp yyuser1@${REMOTE_HOST}:$REMOTE_BASEDIR/soft/tomcat-base/webapps/ROOT.war $LOCAL_BASEDIR/apps/base
-
-echo "====copy sys ROOT.war"
-scp yyuser1@${REMOTE_HOST}:/mnt/syscenter/apache-tomcat-7.0.69/webapps/ROOT.war $LOCAL_BASEDIR/apps/sys
+echo "====copy wars"
+scp -r $REMOTE_USER@${REMOTE_HOST}:$REMOTE_BASEDIR/soft/tomcat-all/webapps $LOCAL_BASEDIR/apps/webapps
 
 echo "====copy nginx web"
-scp -r yyuser1@${REMOTE_HOST}:/mnt/syscenter/web/systemcenter/* $LOCAL_BASEDIR/apps/web/syscenter/
-scp -r yyuser1@${REMOTE_HOST}:/mnt/homecenter/dist/* $LOCAL_BASEDIR/apps/web/homecenter/
+scp -r $REMOTE_USER@${REMOTE_HOST}:$REMOTE_BASEDIR/apps/web/ $LOCAL_BASEDIR/apps/web/
 
 
 echo "### END!"
