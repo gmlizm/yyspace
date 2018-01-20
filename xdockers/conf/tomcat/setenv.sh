a5db0ed24d89dd5969db0de0f00007edb1c0d2d7
gmlizm <gmlizm@gmail.com>
2018-01-20 06:57:25 -0500

    修改docker-compose文件

--- xdockers/conf/tomcat/setenv.sh
+++ xdockers/conf/tomcat/setenv.sh
@@ -1,6 +1,7 @@
-###################################################################
-# JAVA_HOME=$IPHARM_HOME/tool/jdk1.8.0_112
-# JRE_HOME=$JAVA_HOME/jre
-JAVA_OPTS=$JAVA_OPTS" -Dipharm.workdir=$IPHARM_HOME -DIPHARM_HOME=$IPHARM_HOME -DIPHARM_APPDIR=$IPHARM_HOME/apps "
-JAVA_OPTS=$JAVA_OPTS" -server -Xms256m -Xmx256m -Xmn96m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m "
-JAVA_OPTS=$JAVA_OPTS" -XX:ErrorFile=$CATALINA_HOME/logs/java_err_%p.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$CATALINA_HOME/logs "
+#################################################################
+IPHARM_HOME=$IPHARM_HOME
+JAVA_HOME=$IPHARM_HOME/tool/jdk1.8.0_112
+JRE_HOME=$JAVA_HOME/jre
+JAVA_OPTS=" -DIPHARM_HOME=$IPHARM_HOME -server -Xms512m -Xmx512m -Xmn128m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m"
+#################################################################
+#JAVA_OPTS=$JAVA_OPTS" -DIPHARM_APPDIR=$IPHARM_HOME/apps "
